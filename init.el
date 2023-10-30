107822fbff1117fd964162a7d746aee4a4af4374
Gregory DAVID <gregory.david@p1sec.com>
2023-10-30 17:16:40 +0100

    global new conf for an emacs-nox with `gruvbox-dark-hard` tuned

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
-;;; init.el --- Source externe des paquets avec MELPA
+;;; init.el -- Gregory 'gdd' David's Emacs configuration
+;;; Commentary: This is a complete configuration for Emacs >=29
+
 (require 'package)
+;;; Code:
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/")
              )
@@ -11,23 +14,58 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Configuration Générale
-(require 'sr-speedbar)
-(global-set-key (kbd "M-s s") 'sr-speedbar-toggle)
-(global-set-key (kbd "M-b") 'list-buffers)
-(global-set-key (kbd "C-S-s") 'isearch-forward-symbol-at-point)
+(use-package auto-package-update
+    :ensure t
+    :config
+    (setq auto-package-update-delete-old-versions t
+        auto-package-update-interval 4
+        )
+    (auto-package-update-maybe)
+    )
 
+(use-package diff-hl
+    :ensure t
+    :init (diff-hl-mode)
+    )
+
+;; Global Configuration
+(setq rst-pdf-program "zathura")
+(setq inhibit-splash-screen t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(ffap-bindings)
+(global-diff-hl-mode)
+(global-display-line-numbers-mode)
+(column-number-mode 1)
+(electric-pair-mode 1)
+(global-hl-line-mode 1)
+(show-paren-mode 1)
+(subword-mode t)
+(global-auto-revert-mode 1)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+(delete-selection-mode)
+;;; Bindings
+(global-set-key (kbd "M-b") 'list-buffers)
+(global-set-key (kbd "C-S") 'isearch-forward-symbol-at-point)
+
+;; External packages
+;;; Editorconfig
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1)
   )
-;; move-text Alt-(Up|Down)Arrow to move line
+
+;;; move-text Alt-(Up|Down)Arrow to move line
 (use-package move-text
   :config
   (move-text-default-bindings)
   )
 
+;;; Zygospore
 (use-package zygospore
   :bind
   (
@@ -36,23 +74,20 @@
    )
   )
 
-(setq inhibit-splash-screen t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(global-linum-mode 1)
-(column-number-mode 1)
-(show-paren-mode 1)
-(global-auto-revert-mode 1)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
-(delete-selection-mode)
-
-
-(use-package highlight-parentheses)
-(use-package highlight-operators)
-(use-package highlight-numbers)
+(use-package hlinum
+    :ensure t
+    :init
+    (hlinum-activate)
+    )
+(use-package highlight-parentheses
+    :ensure t
+    )
+(use-package highlight-operators
+    :ensure t
+    )
+(use-package highlight-numbers
+    :ensure t
+    )
 
 (define-globalized-minor-mode global-highlight-parentheses-mode
   highlight-parentheses-mode
@@ -90,15 +125,6 @@
     (treemacs-follow-mode t)
     (treemacs-filewatch-mode t)
     (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
 
     (treemacs-hide-gitignored-files-mode nil))
   :bind
@@ -109,30 +135,64 @@
         ("C-x t d"   . treemacs-select-directory)
         ("C-x t B"   . treemacs-bookmark)
         ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
+        ("C-x t M-t" . treemacs-find-tag)
+        )
+  )
+
+(use-package cmake-mode)
+
+(use-package magit
+    :ensure t
+    )
+
+(use-package forge
+    :after magit
+    )
+
+(use-package sqlite3
+    :ensure t
+    :after forge
+    )
 
 (use-package treemacs-magit
-  :after (treemacs magit)
-  :ensure t)
+    :after (treemacs magit)
+    :ensure t
+    )
+
+(add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
 ;; lsp
 (use-package lsp-mode
-  :ensure t
-  :hook (
-         (c-mode . lsp)
-	       (c++-mode . lsp)
-         (python-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration)
-         )
-  :commands lsp
-  :config
-  (setq read-process-output-max (* 1024 1024)
+    :ensure t
+    :hook (
+              (c-mode . lsp-deferred)
+              (c++-mode . lsp-deferred)
+              (python-mode . lsp-deferred)
+              (json-mode . lsp-deferred)
+              (lua-mode . lsp-deferred)
+              (java-mode . lsp-deferred)
+              (js-mode . lsp-deferred)
+              (js-jsx-mode . lsp-deferred)
+              (typescript-mode . lsp-deferred)
+              (web-mode . lsp-deferred)
+              (lsp-mode . lsp-enable-which-key-integration)
+              )
+    :commands lsp lsp-deferred
+    :config
+    (setq
         gc-cons-threshold 100000000
-        lsp-keymap-prefix "C-c l"
+        lsp-clients-clangd-library-directories '("/usr" "/usr/include/c++/10" "/usr/include/c++/11" "/usr/include/c++/12" "/usr/include/c++/13")
+        lsp-enable-imenu nil
         lsp-file-watch-threshold 15000
+        lsp-idle-delay 0.5
+        lsp-keymap-prefix "C-c l"
+        lsp-log-io nil
+        lsp-restart 'auto-restart
+        read-process-output-max (* 1024 1024)
         )
-  (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
-  )
+    (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+    )
 
 (use-package lsp-ui
   :ensure t
@@ -151,16 +211,19 @@
   )
 
 (use-package lsp-ivy
-  :ensure t
-  :commands lsp-ivy-workspace-symbol
-  :config
-  (lsp-treemacs-sync-mode 1))
+    :ensure t
+    :commands lsp-ivy-workspace-symbol
+    :config
+    (lsp-treemacs-sync-mode 1)
+    )
 
 (use-package lsp-treemacs
-  :ensure t
-  :commands lsp-treemacs-errors-list)
+    :ensure t
+    )
+(global-set-key (kbd "C-c l e") 'lsp-treemacs-errors-list)
 
-(use-package dap-mode)
+;; (use-package lsp-java)
+
 (use-package which-key
     :config
     (which-key-mode))
@@ -204,14 +267,6 @@
   :config
   (flycheck-pos-tip-mode))
 
-(use-package srefactor
-  :ensure t
-  :config
-  (semantic-mode 1)
-  (define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-  (define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-  )
-
 (setq auto-mode-alist
       (cons '("SConstruct" . python-mode) auto-mode-alist))
 (setq auto-mode-alist
@@ -241,34 +296,6 @@
                      )
                  )
   )
-
-;; Ouvrir les fichiers .h en c++-mode
-;;(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-;; (use-package function-args
-;;   :config
-;;   (fa-config-default)
-;;   )
-
-;;(use-package helm)
-
-;; (use-package auto-complete-c-headers
-;;   :hook
-;;   (c-mode-common . (lambda ()
-;;                      ;; https://github.com/abo-abo/function-args
-;;                      ;; (function-args-mode t)
-;;                      ;; newline sur certains caractères '{', ';', etc.
-;;                      ;; (c-toggle-auto-state 1)
-;;                      ;; suppression facile des espaces gourmands
-;;                      (c-toggle-hungry-state 1)
-;;                      (add-to-list 'ac-sources 'ac-source-c-headers)
-;;                      (add-to-list 'ac-sources 'ac-source-c-header-symbols t)
-;;                      (add-to-list 'company-c-headers-path-system "/usr/include/c++/10/")
-;;                      (add-to-list 'company-c-headers-path-system "/usr/include/c++/11/")
-;;                      (add-to-list 'company-c-headers-path-system "/usr/include/c++/12/")
-;;                      )
-;;                  )
-;;   )
 
 ;; Semantic modes
 (setq semantic-default-submodes
@@ -302,8 +329,12 @@
 ;;; PlantUML mode
 (use-package plantuml-mode
   :config
-  (setq plantuml-executable-path "/usr/bin/plantuml")
-  (setq plantuml-default-exec-mode 'executable)
+;  (setq plantuml-default-exec-mode 'jar)
+;  (setq plantuml-jar-path '/home/gregory/plantuml.jar)
+;  (setq plantuml-jar-args '-tsvg)
+;  (setq plantuml-java-args '(-Djava.awt.headless=true -jar))
+;;  (setq plantuml-executable-path "/usr/bin/plantuml")
+;;  (setq plantuml-executable-args "-headless -tsvg")
   )
 (use-package flycheck-plantuml
   :config
@@ -331,44 +362,48 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(TeX-view-program-selection
-   '(((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "Zathura")
-     (output-html "xdg-open")))
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
- '(ansi-term-color-vector
-   [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
- '(custom-enabled-themes '(one-light))
- '(custom-safe-themes
-   '("5b89b65f5e9e30d98af9d851297ee753e28528676e8ee18a032934a12762a5f2" "854f0e982e9f46844374b7d72c0137276621db317738281888f15fddb1565aeb" "e7ba99d0f4c93b9c5ca0a3f795c155fa29361927cadb99cfce301caf96055dfd" "b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" "0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197" "76b4632612953d1a8976d983c4fdf5c3af92d216e2f87ce2b0726a1f37606158" default))
- '(fci-rule-character-color "#d9d9d9")
- '(fci-rule-color "#d9d9d9")
- '(flycheck-checker-error-threshold 500)
+      '(((output-dvi has-no-display-manager)
+            "dvi2tty")
+           ((output-dvi style-pstricks)
+               "dvips and gv")
+           (output-dvi "xdvi")
+           (output-pdf "Zathura")
+           (output-html "xdg-open")))
+ '(comment-empty-lines 'eol)
+ '(comment-multi-line t)
+ '(custom-enabled-themes '(gruvbox-dark-hard))
+ '(custom-safe-themes t)
+ '(electric-pair-pairs '((34 . 34) (8216 . 8217) (8220 . 8221) (96 . 96)))
+ '(electric-pair-skip-whitespace-chars '(32 9 10))
+ '(electric-pair-text-pairs '((34 . 34) (8216 . 8217) (8220 . 8221) (96 . 96)))
+ '(ezimage-use-images nil)
+ '(flycheck-checker-error-threshold 5000)
+ '(flycheck-flake8rc "~/.flake8rc")
+ '(flycheck-indication-mode 'left-fringe)
+ '(flycheck-locate-config-file-functions
+      '(flycheck-locate-config-file-home flycheck-locate-config-file-ancestor-directories flycheck-locate-config-file-by-path))
  '(gdb-many-windows t)
  '(indent-tabs-mode nil)
  '(ispell-dictionary nil)
  '(js-indent-level 2)
+ '(lsp-clients-pylsp-library-directories '("/usr/"))
+ '(lsp-document-sync-method nil)
+ '(lsp-pyls-plugins-flake8-ignore '("E501"))
+ '(lsp-pylsp-plugins-flake8-ignore ["E501"])
  '(package-selected-packages
-   '(dap-mode django-commands company-auctex company-reftex company-shell company ac-c-headers ac-clang ac-rtags auto-complete auto-complete-c-headers sr-speedbar mermaid-mode aircon-theme iodine-theme twilight-theme one-themes twilight-bright-theme realgud realgud-ipdb realgud-lldb realgud-node-debug realgud-node-inspect editorconfig editorconfig-charset-extras editorconfig-custom-majormode editorconfig-domain-specific editorconfig-generate groovy-mode jenkinsfile-mode company-rtags disaster ecb flycheck-projectile flycheck-rtags helm helm-ag helm-flycheck helm-projectile helm-rtags projectile projectile-git-autofetch rtags srefactor smart-tabs-mode plantuml-mode magit auctex-lua company-lua flymake-lua lua-mode idle-highlight-mode use-package))
- '(plantuml-default-exec-mode 'executable)
- '(plantuml-executable-args '("-headless -tsvg"))
- '(plantuml-executable-path "/usr/bin/plantuml")
- '(sr-speedbar-skip-other-window-p t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monospace" :foundry "PfEd" :slant normal :weight normal :height 95 :width normal))))
- '(mode-line ((t (:background "cornflower blue" :foreground "#494B53" :box (:line-width 1 :color "#F0F0F0") :weight normal))))
- '(mode-line-inactive ((t (:inherit mode-line :background "light steel blue" :foreground "#A0A1A7" :weight normal))))
- '(sphinx-code-block-face ((t (:inherit fixed-pitch :inverse-video t)))))
+      '(gruvbox-theme dockerfile-mode lsp-mode sqlite3 highlight-function-calls hl-block-mode magit-todos diff-hl emacsql-sqlite graphviz-dot-mode cmake-mode json-mode subed pdf-tools dap-mode django-commands company-auctex company-reftex company-shell company ac-c-headers ac-clang ac-rtags auto-complete auto-complete-c-headers sr-speedbar mermaid-mode aircon-theme iodine-theme twilight-theme one-themes twilight-bright-theme realgud realgud-ipdb realgud-lldb realgud-node-debug realgud-node-inspect editorconfig editorconfig-charset-extras editorconfig-custom-majormode editorconfig-domain-specific editorconfig-generate groovy-mode jenkinsfile-mode company-rtags disaster ecb flycheck-projectile flycheck-rtags helm helm-ag helm-flycheck helm-projectile helm-rtags projectile projectile-git-autofetch rtags srefactor smart-tabs-mode plantuml-mode magit auctex-lua company-lua flymake-lua lua-mode idle-highlight-mode use-package))
+ '(plantuml-default-exec-mode 'jar)
+ '(plantuml-executable-args '("-charset" "UTF-8" "-tsvg"))
+ '(plantuml-indent-level 2)
+ '(plantuml-jar-args '("-charset" "UTF-8" "-tpng"))
+ '(plantuml-java-args '("-Djava.awt.headless=true" "-jar"))
+ '(python-shell-interpreter "ipython3")
+ '(python-shell-interpreter-args "-i --simple-prompt")
+ '(rst-compile-toolsets '((html "rst2html" ".html" nil) (pdf "rst2pdf" ".pdf" nil)))
+ '(split-height-threshold nil)
+ '(split-width-threshold 160)
+ '(warning-suppress-log-types '((comp) (use-package) (emacsql)))
+ '(warning-suppress-types '((emacsql))))
 
 (global-set-key (kbd "C-M-<left>")  'windmove-left)
 (global-set-key (kbd "C-M-<right>") 'windmove-right)
@@ -377,7 +412,7 @@
 
 ;;; DevHelp sur le mot via <f7>
 (defun devhelp-word-at-point ()
-  "Cherche dans DevHelp le mot courant sous le curseur"
+  "Cherche dans DevHelp le mot courant sous le curseur."
   (interactive)
   (start-process-shell-command "devhelp" nil (concat "devhelp" " -s " (current-word)))
   (set-process-query-on-exit-flag (get-process "devhelp") nil)
@@ -394,5 +429,83 @@
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 (global-set-key (kbd "C-c t") 'whitespace-toggle-options)
 
+;;; Comments
+(global-set-key (kbd "C-M-;") 'uncomment-region)
+
+;;; Duplicate line with C-d
+(defun duplicate-line-or-region (&optional n)
+  "Duplicate current line, or region if active.
+    With argument N, make N copies.
+    With negative N, comment out original line and use the absolute value."
+  (interactive "*p")
+  (let ((use-region (use-region-p)))
+    (save-excursion
+      (let ((text (if use-region        ;Get region if active, otherwise line
+                      (buffer-substring (region-beginning) (region-end))
+                    (prog1 (thing-at-point 'line)
+                      (end-of-line)
+                      (if (< 0 (forward-line 1)) ;Go to beginning of next line, or make a new one
+                          (newline)
+                        )
+                      )
+                    )
+                  )
+            )
+        (dotimes (i (abs (or n 1)))     ;Insert N times, or once if not specified
+          (insert text)
+          )
+        )
+      )
+    (if use-region nil                  ;Only if we're working with a line (not a region)
+      (let ((pos (- (point) (line-beginning-position)))) ;Save column
+        (if (> 0 n)                             ;Comment out original with negative arg
+            (comment-region (line-beginning-position) (line-end-position)))
+        (forward-line 1)
+        (forward-char pos)
+        )
+      )
+    )
+  )
+(global-set-key [C-d] 'duplicate-line-or-region)
+
+(defun decrement-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+          (error "No number at point"))
+  (replace-match (number-to-string (- (string-to-number (match-string 0)) 1))))
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+          (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+(global-set-key (kbd "M-<left>") 'decrement-number-at-point)
+(global-set-key (kbd "M-<right>") 'increment-number-at-point)
+
+(add-hook 'prog-mode-hook #'(lambda () (hs-minor-mode t)))
+(global-set-key (kbd "C-<prior>") 'hs-hide-block)
+(global-set-key (kbd "C-<next>") 'hs-show-block)
+(global-set-key (kbd "C-S-<next>") 'hs-show-all)
+
 (provide 'init)
 ;;; init.el ends here
+(put 'magit-diff-edit-hunk-commit 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "color-232" :foreground "brightwhite" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(line-number ((t (:background "color-235" :foreground "#767676"))))
+ '(line-number-current-line ((t (:background "#4e4e4e" :foreground "#ff8700" :weight bold))))
+ '(magit-diff-added ((t (:extend t :foreground "brightgreen"))))
+ '(magit-diff-added-highlight ((t (:inherit (magit-diff-added magit-diff-context-highlight) :foreground "brightgreen"))))
+ '(magit-diff-context ((t (:extend t :foreground "white"))))
+ '(magit-diff-context-highlight ((t (:extend t :background "#3a3a3a" :foreground "white"))))
+ '(magit-diff-file-heading ((t (:extend t :background "white" :foreground "black" :weight bold))))
+ '(magit-diff-file-heading-highlight ((t (:inherit magit-diff-file-heading :extend t))))
+ '(magit-diff-hunk-region ((t (:inherit bold :extend t :background "black" :slant italic))))
+ '(mode-line ((t (:background "color-178" :foreground "color-232" :box nil))))
+ '(mode-line-inactive ((t (:background "color-94" :foreground "color-252" :box nil))))
+ '(region ((t (:extend t :background "color-234")))))
